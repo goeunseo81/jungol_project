783015ee24c90a870c9ab9e97c19fa39a815d220
고은서 <goeunseo1004@gmail.com>
2026-09-05 17:20:46 +0900

    9월 첫째주 숙제 수정본
    제목동일

--- 9월첫째주숙제.py
+++ 9월첫째주숙제.py
@@ -1,3 +1,4 @@
+'''
 import sys
 input = sys.stdin.readline
 T = int(input())
@@ -5,3 +6,22 @@ T = int(input())
 for i in range(T):
     a,b =  map(int, input().split())
     print(a**b%10)
+'''
+import sys
+import math
+input = sys.stdin.readline #입력 속도 빠르게
+T = int(input())
+
+for i in range(T):
+    a,b =  map(int, input().split())
+
+    #a와 b의 제곱을 10으로 나눈 나머지를 구합니다
+    #pow쪽이 a**b%10보다 빠릅니다
+    result = pow(a, b, 10)
+
+    #나머지가 0이면 10번 컴퓨터입니다
+    if result == 0:
+        print(10)
+
+    else:
+        print(result)
