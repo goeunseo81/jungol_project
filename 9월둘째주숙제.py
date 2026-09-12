337c8f10d7b900d02e7d830deecc69af4b9e36b6
고은서 <goeunseo1004@gmail.com>
2026-09-12 17:10:57 +0900

    9월둘째주숙제
    제목대로

--- 9월둘째주숙제.py
+++ 9월둘째주숙제.py
@@ -3,14 +3,14 @@ from itertools import permutations
 n_list = [0, 1, 2, 3, 4, 5]
 r_list = list(permutations(n_list,4))
 
-max_val = -float('inf')
+min_val = float('inf')
 for i in r_list:
     s=0
     for j in range(4):
         s+=i[j]*(10**j)
-    if s>max_val and s%6==0:
-        max_val = s
-print(max_val)
+    if s < min_val and s%6==0 and s>=1000:
+        min_val = s
+print(min_val)
 
 from itertools import combinations
 n_list = [10, 20, 33, 40, 55, 61, 78, 80, 91, 100]
